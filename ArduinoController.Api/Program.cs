8dba4c1e99187df1eed27189c8928cb65e468c4a
Marcin Lewandowski <marcin.lewandowski996@gmail.com>
2018-11-09 16:42:18 +0100

    Added Application Insights

--- ArduinoController.Api/Program.cs
+++ ArduinoController.Api/Program.cs
@@ -19,6 +19,7 @@ namespace ArduinoController.Api
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
